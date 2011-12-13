2a031ed7990484ddbcbcde7a44eb82fae67c6e12
Roman Baron <roman.baron@gmail.com>
2011-12-13 17:20:31 +0200

    Added a site layout

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.1.3'
 gem 'therubyracer'
 gem 'jquery-rails'
 
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
@@ -23,7 +24,8 @@ end
 
 
 group :test do
-  # Pretty printed test output
+  gem 'nokogiri'
+  gem 'webrat'
   gem "rspec"
   gem 'turn', '0.8.2', :require => false
   gem 'sqlite3'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector('title',
+                                    :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
+    it "should have a non blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -16,4 +28,11 @@ describe PagesController do
     end
   end
 
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
